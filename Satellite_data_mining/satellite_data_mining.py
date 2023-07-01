bf17bb8321cc18569e240af3f3c6c34288613566
Nadav <navrahami@paypal.com>
2023-07-01 19:10:38 +0300

    github

--- Satellite_data_mining/satellite_data_mining.py
+++ Satellite_data_mining/satellite_data_mining.py
@@ -1,7 +1,6 @@
 import logging
 import sys
 import os
-import boto3
 
 import pystac_client
 
@@ -434,30 +433,3 @@ if __name__ == '__main__':
     time_range = datetime.today().strftime('%Y-%m-%d')
     # time_range = '2023-06-14'
     main(time_range)
-
-
-def get_secret():
-
-    secret_name = "NDVI-backend-api"
-    region_name = "us-east-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    # Decrypts secret using the associated KMS key.
-    secret = get_secret_value_response['SecretString']
-
-    # Your code goes here.
